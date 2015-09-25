2ad86b255c6ac2336abfa5e0adde36ab8a44dbfe
mascaaj <alokejm@gmailcom>
2015-09-25 11:54:18 -0500

    This file takes a square matrix and returns the inverse using the solve function.
    If the inverse is generated in by the makeCacheMatrix function, the cacheSolve function will obtain the answer from the cache, else it will solve the inverse independently.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat<-NULL
+  set<-function(y){
+    mat<<-NULL
+    x<<-y
+  }
+  get<-function() x
+  setinv<-function(lim) mat<<-solve(lim)
+  getinv<-function() mat
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
@@ -12,4 +20,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat<-x$getinv()
+  #jim<-solve(x$get)
+  if(!is.null(mat)){
+    #if(identical(solve(jim),mat,num.eq=TRUE))
+    {
+      message("getting cached data")
+      return(mat)
+    }
+  }
+  data<-x$get()
+  mat<-x$setinv(data)
+  mat
 }
